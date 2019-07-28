a6bbc7dd14192a714aa0076e2b98665372757412
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2019-07-28 09:50:23 +0100

    front end created

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'));
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+  }); 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
