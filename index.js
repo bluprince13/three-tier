a5053325044f829090856af8ea76e686c4060e98
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2019-07-28 10:02:58 +0100

    redirect to homepage after post

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const bodyParser= require('body-parser')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'));
-app.get('/', function(req, res){
-    res.sendFile('index.html');
-  }); 
+app.use(express.static("public"));
+app.get("/", function(req, res) {
+	res.sendFile("index.html");
+});
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
-console.log(req.body)
-})
+app.post("/", (req, res) => {
+	console.log(req.body);
+	res.redirect("/");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
