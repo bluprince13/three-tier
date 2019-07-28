dfaf9e18b0249a89e7e8975649648694f955b025
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2019-07-28 09:58:43 +0100

    post name

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser= require('body-parser')
 const app = express()
 const port = 3000
 
@@ -6,5 +7,11 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.sendFile('index.html');
   }); 
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/', (req, res) => {
+console.log(req.body)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
